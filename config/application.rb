c0b5e3d39147fc2473c8030d1b6a8fd3d6d169b6
money2868 <chenglong3389@gmail.com>
2016-08-09 03:23:25 +0800

    增加了测试内容
    第14行

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Railsblog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # 随便写点内容。测试github
   end
 end
