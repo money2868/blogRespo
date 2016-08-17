edf8d1e99baaa51be7149ed5476860b4e6c9dd79
money2868 <chenglong3389@gmail.com>
2016-08-17 23:52:35 +0800

    mod puma path

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env puma
 
 #rails的运行环境
-#environment 'production'
-environment 'development'
+environment 'production'
+#environment 'development'
 threads 2, 64
 workers 4
 
@@ -14,11 +14,11 @@ application_path = "/var/www/#{app_name}"
 directory "#{application_path}/current"
 
 #下面都是 puma的配置项
-pidfile "#{application_path}/shared/tmp/pids/puma.pid"
-state_path "#{application_path}/shared/tmp/sockets/puma.state"
-stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
-bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
-activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
+pidfile "#{application_path}/tmp/pids/puma.pid"
+state_path "#{application_path}/tmp/sockets/puma.state"
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+bind "unix://#{application_path}/tmp/sockets/#{app_name}.sock"
+activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
 
 #后台运行
 daemonize true
